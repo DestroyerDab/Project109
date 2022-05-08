1f60e78805eda1c13d03c724861392bfacb87825
Manjot S <73439030+DestroyerDab@users.noreply.github.com>
2022-05-08 18:00:11 -0500

    Add files via upload

---
+++ Class-109/code.py
@@ -0,0 +1,32 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import pandas as pd
+import statistics
+
+df = pd.read_csv("StudentsPerformance.csv")
+ms = df["math score"].tolist()
+fig = ff.create_distplot([ms], ["MS"], show_hist=False)
+
+
+mean = statistics.mean(ms)
+sd = statistics.stdev(ms)
+
+first_st_deviation_start, first_st_deviation_end = mean-sd, mean+sd
+second_st_deviation_start, second_st_deviation_end = mean-(2*sd), mean+(2*sd)
+third_st_deviation_start, third_st_deviation_end = mean-(3*sd), mean+(3*sd)
+
+fig.add_trace(go.Scatter(x=[mean, mean], y = [0, 0.05], mode="lines", name="Mean"))
+fig.add_trace(go.Scatter(x=[first_st_deviation_start, first_st_deviation_start], y = [0, 0.05], mode="lines", name="FirstDevationStart"))
+fig.add_trace(go.Scatter(x=[first_st_deviation_end, first_st_deviation_end], y = [0, 0.05], mode="lines", name="FirstDevationEnd"))
+fig.add_trace(go.Scatter(x=[second_st_deviation_start, second_st_deviation_start], y = [0, 0.05], mode="lines", name="SecondDevationStart"))
+fig.add_trace(go.Scatter(x=[second_st_deviation_end, second_st_deviation_end], y = [0, 0.05], mode="lines", name="SecondDevationEnd"))
+fig.show()
+
+data_in_1st_st_deviation = [result for result in ms if result > first_st_deviation_start and result < first_st_deviation_end]
+data_in_2nd_st_deviation = [result for result in ms if result > second_st_deviation_start and result < second_st_deviation_end]
+data_in_3rd_st_deviation = [result for result in ms if result > third_st_deviation_start and result < third_st_deviation_end]
+print("Mean of this data is {}".format(mean))
+print("Standard Deviation is {}".format(sd))
+print("{}% of data lies within 1 standard deviation".format(len(data_in_1st_st_deviation)*100.0/len(ms)))
+print("{}% of data lies within 2 standard deviation".format(len(data_in_2nd_st_deviation)*100.0/len(ms)))
+print("{}% of data lies within 3 standard deviation".format(len(data_in_3rd_st_deviation)*100.0/len(ms)))
